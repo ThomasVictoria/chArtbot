3eccef5c2b9593bd022f9610956d65077245ff25
Thomas Victoria <thomas@MacBook-Pro-de-Thomas.local>
2017-06-11 22:29:27 +0200

    croon

--- app/cron.js
+++ app/cron.js
@@ -3,7 +3,7 @@ const cron = require('node-cron');
 module.exports = {
 	init: function(){
 		console.log('Cron task scheduled')
-		cron.schedule('59 23 * * *', function(){
+		cron.schedule('40 * * * *', function(){
 		  console.log('Running Cron task');
 		  scrapr.init()
 		});

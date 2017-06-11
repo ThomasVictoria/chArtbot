02e4e4aa5535d00bd92200196014e11fc2605d3f
Thomas Victoria <thomas@MacBook-Pro-de-Thomas.local>
2017-06-11 22:49:47 +0200

    scrapr bug

--- app/cron.js
+++ app/cron.js
@@ -1,9 +1,10 @@
-const cron = require('node-cron');
+const cron    = require('node-cron')
+	  scrapr = require('../scrapr/scraper');
 
 module.exports = {
 	init: function(){
 		console.log('Cron task scheduled')
-		cron.schedule('40 * * * *', function(){
+		cron.schedule('55 * * * *', function(){
 		  console.log('Running Cron task');
 		  scrapr.init()
 		});
